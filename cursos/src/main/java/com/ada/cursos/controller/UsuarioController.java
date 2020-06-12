140513e2a994dda855468de661f4947f7e63e54d
Lorena Arabia <56326571+Larabia@users.noreply.github.com>
2020-06-12 14:23:29 -0300

    se agrega la entidad alumno, con la clase datos SE como atributo, metodo POST de alumno y test. Los test de usuario y alumno dan error cuando pasa el requestbody, tengo que revisarlo

--- cursos/src/main/java/com/ada/cursos/controller/UsuarioController.java
+++ cursos/src/main/java/com/ada/cursos/controller/UsuarioController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import com.ada.cursos.form.UsuarioForm;
+import com.ada.cursos.form.LoginForm;
 import com.ada.cursos.model.Usuario;
 import com.ada.cursos.repository.UsuarioRepository;
 import io.swagger.v3.oas.annotations.Operation;
@@ -27,13 +27,13 @@ public class UsuarioController {
 	@PostMapping(path = "/alta")
 	@Operation (summary = "Alta usuario", description = "Ingrasa un objeto usuario a la base de datos")
 	
-	public ResponseEntity<Usuario> altaUsuairo(@RequestBody UsuarioForm usuarioForm) {
+	public ResponseEntity<Usuario> altaUsuairo(@RequestBody LoginForm loginForm) {
 				
 		log.info("metodo: altaUsuario.");
 		
 		Usuario usuario = new Usuario();
-		usuario.setEmail(usuarioForm.getEmail());
-		usuario.setPass(usuarioForm.getPass());
+		usuario.setEmail(loginForm.getEmail());
+		usuario.setPass(loginForm.getPass());
 		
 		usuarioRepo.save(usuario);
 		log.info("metodo: Usuario guardado.");
--- cursos/src/test/java/com/ada/cursos/AlumnoTest.java
+++ cursos/src/test/java/com/ada/cursos/AlumnoTest.java
@@ -2,32 +2,28 @@ package com.ada.cursos;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
-
-import java.util.HashMap;
-import java.util.Map;
-
 import org.junit.jupiter.api.Test;
 import com.mashape.unirest.http.HttpResponse;
 import com.mashape.unirest.http.JsonNode;
 import com.mashape.unirest.http.Unirest;
 import com.mashape.unirest.http.exceptions.UnirestException;
 
-public class UsuarioTest {
-
+public class AlumnoTest {
+	
 	@Test
 	public void altaUsuario() throws UnirestException {
-		
-	    Map<String, Object> fields = new HashMap<>();
-	    fields.put("id", "2");
-	    fields.put("email", "test2@mock.com");
 	    
 	    HttpResponse<JsonNode> jsonResponse 
-	      = Unirest.post("http://localhost:8080/usuarioController/alta")
+	      = Unirest.post("http://localhost:8080/alumnoController/alta")
 	      .header("accept", "application/json").body("{\"id\":\"2\","
-		      		+ " \"email\":\"test2@mock.com\"}")
-		      .asJson();
+	      		+ " \"nombre\":\"mock2\","
+	      		+ " \"apellido\":\"mock22\","
+	      		+ " \"estudia\":\"true\","
+	      		+ " \"trabaja\":\"false\"}")
+	      .asJson();
 	 
 	    assertNotNull(jsonResponse.getBody());
 	    assertEquals(200, jsonResponse.getStatus());
 	}
+
 }
