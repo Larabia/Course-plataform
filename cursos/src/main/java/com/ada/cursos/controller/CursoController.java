56379c414c35a1e67dc92ec54c5fe8405cbbe7ea
Lorena Arabia <56326571+Larabia@users.noreply.github.com>
2020-06-15 14:57:06 -0300

    metodos post, delete y cursoPorId agregados a CursoControler.Se refactorean las capas service y controller de Curso.

--- cursos/src/main/java/com/ada/cursos/controller/CursoController.java
+++ cursos/src/main/java/com/ada/cursos/controller/CursoController.java
@@ -1,12 +1,12 @@
 package com.ada.cursos.controller;
 
 import java.util.List;
-import java.util.Optional;
 import java.util.logging.Logger;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -40,12 +40,22 @@ public class CursoController {
 	private EmpresaRepository empRepo;
 
 	Logger log = Logger.getLogger(CursoRepository.class.getName());
+	
+	@GetMapping(path = "/{id}")
+	@Operation(summary = "cursoPorId", description = "Recibe como un Long id y devuelve el Curso con el id correspondiente.")
+	public ResponseEntity<Curso> cursoPorId(@PathVariable Long id) {
+
+		log.info("Buscando curso id"+id);
+		Curso curso = cursoServ.porId(id);		
+
+		return new ResponseEntity<>(curso, HttpStatus.OK);
+	}
 
 	@GetMapping(path = "/listado")
-	@Operation(summary = "ListarCursos", description = "Trae una lista de cursos en la base")
+	@Operation(summary = "listarCursos", description = "Lista todos los cursos presentes en la base de datos.")
 	public ResponseEntity<List<Curso>> listarCursos() {
 
-		log.info("comienzo invocacion listar cursos...");
+		log.info("listando cursos...");
 
 		Iterable<Curso> ListCurIt = cursoRepo.findAll();
 		List<Curso> cursosAbiertos = Lists.newArrayList(ListCurIt);
@@ -54,10 +64,10 @@ public class CursoController {
 	}
 
 	@GetMapping(path = "/listado-abiertos")
-	@Operation(summary = "ListarAbiertos", description = "Trae una lista de cursos abiertos")
+	@Operation(summary = "listarAbiertos", description = "Lista todos los cursos que coinciden con abierto=true.")
 	public ResponseEntity<List<Curso>> listarAbiertos() {
 
-		log.info("comienzo invocacion listar cursos...");
+		log.info("listando cursos abiertos...");
 
 		Iterable<Curso> ListCurIt = cursoRepo.findByAbiertoTrue();
 		List<Curso> cursosAbiertos = Lists.newArrayList(ListCurIt);
@@ -66,10 +76,10 @@ public class CursoController {
 	}
 
 	@GetMapping(path = "/listado-por-categoria")
-	@Operation(summary = "ListarPorCategoria", description = "Trae una lista de cursos por categoria")
+	@Operation(summary = "listarPorCategoria", description = "Recibe como parámetro un String categoría y lista todos los cursos cuya categoría coincide.")
 	public ResponseEntity<List<Curso>> listarPorCategoria(@RequestParam String categoria) {
 
-		log.info("comienzo invocacion listar cursos...");
+		log.info("listando cursos de la categoria " + categoria + "...");
 
 		Iterable<Curso> ListCurIt = cursoRepo.findByCategoriaStartingWith(categoria);
 		List<Curso> cursos = Lists.newArrayList(ListCurIt);
@@ -92,14 +102,14 @@ public class CursoController {
 	}
 
 	@PostMapping(path = "/alta")
-	@Operation(summary = "updateCurso", description = "Modifica un objeto curso a la base de datos")
+	@Operation(summary = "altaCurso", description = "Persiste un nuevo Curso en la base de datos.")
 
 	public ResponseEntity<Curso> altaCurso(@RequestBody CursoForm cursoForm) {
 
-		log.info("metodo: altaCurso.");
+		log.info("creando curso...");
 
 		Curso curso = new Curso();
-		curso =	cursoServ.generarCursoDeForm(cursoForm, curso);
+		curso = cursoServ.generarCursoDeForm(cursoForm, curso);
 		cursoRepo.save(curso);
 
 		log.info("metodo: Curso guardado.");
@@ -109,27 +119,33 @@ public class CursoController {
 	}
 
 	@PutMapping(path = "/update/{id}")
-	@Operation(summary = "updateCurso", description = "Modifica un objeto curso a la base de datos")
-
+	@Operation(summary = "updateCurso", description = "Recibe un Long id y un CursoForm, busca el curso por id y lo actualiza con los datos del formulario.")
 	public ResponseEntity<Curso> updateCurso(@RequestBody CursoForm cursoForm, @PathVariable Long id) {
-		log.info("metodo: updateCurso.");
+		log.info("Buscando curso...");
 
-		Optional<Curso> cursoOp = cursoRepo.findById(id);
+		Curso curso = cursoServ.porId(id);
+		log.info("Modificando curso...");
+		
+		curso = cursoServ.generarCursoDeForm(cursoForm, curso);
+		cursoRepo.save(curso);
+		
+		log.info("Curso modificado.");
+		return new ResponseEntity<>(curso, HttpStatus.OK);
 
-		if (Optional.empty().equals(cursoOp)) {
+	}
 
-			log.info("metodo: El id ingresado no existe.");
-			return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
+	@DeleteMapping(path = "/delete/{id}")
+	@Operation(summary = "deleteCurso", description = "Recibe un Long id, busca el curso por id y borra de la base de datos.")
+	public ResponseEntity<Object> deleteCurso(@PathVariable Long id) {
+		
+		log.info("Buscando curso...");
+		Curso curso = cursoServ.porId(id);
+		
+		log.info("Borrando curso id  " + id);
+		cursoRepo.delete(curso);
 
-		} else {
-			Curso curso = cursoOp.get();
-			curso = cursoServ.generarCursoDeForm(cursoForm, curso);
-			cursoRepo.save(curso);
-			log.info("metodo: Curso modificado.");
-			return new ResponseEntity<>(curso, HttpStatus.OK);
-
-		}
-
+		log.info("Curso borrado.");
+		return new ResponseEntity<>(null, HttpStatus.OK);
 	}
 
 }
--- cursos/src/main/java/com/ada/cursos/service/CursoService.java
+++ cursos/src/main/java/com/ada/cursos/service/CursoService.java
@@ -10,36 +10,39 @@ import com.ada.cursos.form.CursoForm;
 import com.ada.cursos.model.Curso;
 import com.ada.cursos.model.Empresa;
 import com.ada.cursos.repository.CursoRepository;
-import com.ada.cursos.repository.EmpresaRepository;
+
+
 
 @Service
 public class CursoService {
 
+	
 	@Autowired
-	private EmpresaRepository empRepo;
+	private EmpresaService empService;
 
 	@Autowired
 	private CursoRepository cursoRepo;
-	
+
 	Logger log = Logger.getLogger(CursoRepository.class.getName());
 
+	
+	
 	public Curso porId(Long id) {
-				
-		java.util.Optional<Curso> cursoOp = cursoRepo.findById(id);	
-		
-	    if (Optional.empty().equals(cursoOp)) {			
-	    	log.info("El id ingresado no existe.");
-	    	
+
+		Optional<Curso> cursoOp = cursoRepo.findById(id);
+
+		if (Optional.empty().equals(cursoOp)) {
+			log.info("El id ingresado no existe.");
 		}
 
-	    Curso curso = cursoOp.get();	    
-	    return curso;
-		
+		Curso curso = cursoOp.get();
+		return curso;
+
 	}
 
 	public Curso generarCursoDeForm(CursoForm cursoForm, Curso curso) {
-
-		Empresa empresa =  empRepo.findById(cursoForm.getEmpId()).get();
+		
+		Empresa empresa = empService.porId(cursoForm.getEmpId());
 
 		curso.setNombre(cursoForm.getNombre());
 		curso.setHoras(cursoForm.getHoras());
